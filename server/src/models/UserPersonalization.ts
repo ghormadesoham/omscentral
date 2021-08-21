75a249a96c650fceb55f0f4dc3b9e8a50e0a12c9
Soham <ghormade.soham@gmail.com>
2021-08-21 15:21:41 -0400

    Issue 58:Notifications: add TODOs for next steps.

--- server/src/models/UserPersonalization.ts
+++ server/src/models/UserPersonalization.ts
@@ -10,7 +10,9 @@ export class UserPersonalization extends Domain {
   user_id!: string;
 
   static tableName = 'omscentral_user_personalization';
-
+  // TODO: 1: one to many relationship between user and user_personalization
+  // TODO: 2: what to use as the id for user_personalization? generate id?
+  // TODO: currently using user id but user id will not work once (1) above is addressed.
   static relationMappings = {
     user: {
       relation: Domain.HasOneRelation,
