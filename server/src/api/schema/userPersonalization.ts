74c2665b0428dd7ff84fe774a259785cca1c62a2
Soham <ghormade.soham@gmail.com>
2021-12-26 14:13:47 -0500

    Issue 58:Server: update schema.graphl and api/schema/userPerosnalization.ts given the change in DB PR: specifically make type and value as not nullable. Remove auth check in upsert for user personalization since the user will already be authenticated at that point. Remove the redundant TODO comment. Verify that the website builds and runs without issue locally.

--- server/src/api/schema/userPersonalization.ts
+++ server/src/api/schema/userPersonalization.ts
@@ -3,6 +3,6 @@ import Joi from 'joi';
 export const schema = Joi.object().keys({
   id: Joi.string().required(),
   user_id: Joi.string().required(),
-  type: Joi.string().allow(null),
-  value: Joi.string().allow(null),
+  type: Joi.string().required(),
+  value: Joi.string().required(),
 });
